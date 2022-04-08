6cc3d5c58c0fcde13ee1944f3c34db5f8c9f3283
lealalves <leealalves@hotmail.com>
2022-04-08 00:27:44 -0300

    menu dropdown for mobile version
    feito menu dropdown para mobile, também mexi um pouco na estética do header, alterando algumas posições e principalmente a logo xD

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <li @click="isOpen = !isOpen">{{title}}</li>
+    <transition name="fade" appear>
+      <div class="sub-menu" v-show="isOpen" >
+        <div v-for="(item, i) in items" :key="i">
+          <component 
+            :is="item.is" 
+            :to="item.url"
+            @click="detectEvent($event)">{{item.title}}
+          </component>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: ['title', 'items'],
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    detectEvent(e){
+      if(e.target.nodeName == 'P'){
+        this.logout()
+      }
+    },
+    async logout() {
+      const req = await fetch(`${process.env.VUE_APP_API_URL}usuarios/logout`);
+
+      const res = await req.json();
+
+      if(!res.ok) {
+        this.msgs = res;
+      }else {
+        this.$router.push({
+          name: "Home",
+          params: {
+            ok: res.texto,
+          },
+        });
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.sub-menu {
+  position: absolute;
+  background-color: #222;
+  right: 5px;
+  border-radius: 10px;
+}
+.sub-menu div{
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .3s ease-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,117 @@
 <template>
   <div id="header">
-    <ul id="header_links">
-      <router-link to="/"><li><h3>Blog do Node</h3></li></router-link>
+    <router-link :to="{name: 'Home'}"><img id="imgLogo" src="../assets/logo.jpg" alt="" srcset=""></router-link>
+    <ul v-show="!isMobile" id="header_links">
       <router-link to="/"><li>Home</li></router-link>
       <router-link to="/categorias"><li>Categorias</li></router-link>
       <router-link v-show="user_data && user_data.eAdmin == 1" to="/postagens"><li>Postagens</li></router-link>
       <router-link v-show="user_data && user_data.eAdmin == 1" to="/listacategorias"><li>Lista de Categorias</li></router-link>
-      <router-link v-show="!user_data" to="/login"><li>Login</li></router-link>
-      <li v-show="user_data" style="cursor: pointer" @click="logout()">Sair</li>
-      <router-link v-show="!user_data" to="/registro"><li>Registro</li></router-link>
+      <router-link v-show="!isAuth" to="/login"><li>Login</li></router-link>
+      <router-link v-show="!isAuth" to="/registro"><li>Registrar</li></router-link>
     </ul>
     <ul id="header_user">
-      <li>{{user_data ? user_data.nome : ''}}</li>
+      <Dropdown
+      v-show="isMobile || isAuth"
+      :title="isAuth ? `Bem-vindo, ${user_data.nome}!` : 'Menu'" 
+      :items="setItems"
+      />
     </ul>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
+import Dropdown from './Dropdown.vue'
+
+
 export default {
   name: "Header",
+  components: {
+    Dropdown
+  },
   data() {
     return {
-      user_data: null
+      user_data: null,
+      windowWidth: ref(window.innerWidth),
+      desktop: [
+        {
+          title: 'Sair',
+          is: 'p'
+        },
+      ],
+      responsiveLogged: [
+        {
+          title: 'Home',
+          is: 'router-link',
+          url: '/'
+        },
+        {
+          title: 'Postagens',
+          is: 'router-link',
+          url: '/postagens'
+        },
+        {
+          title: 'Categorias',
+          is: 'router-link',
+          url: '/categorias',
+        },
+        {
+          title: 'Lista de Categorias',
+          is: 'router-link',
+          url: '/listacategorias',
+        },
+        {
+          title: 'Sair',
+          is: 'p',
+        }
+      ],
+      responsiveOffline: [
+        {
+          title: 'Home',
+          is: 'router-link',
+          url: '/'
+        },
+        {
+          title: 'Postagens',
+          is: 'router-link',
+          url: '/postagens'
+        },
+        {
+          title: 'Categorias',
+          is: 'router-link',
+          url: '/categorias',
+        },
+        {
+          title: 'Registrar',
+          is: 'router-link',
+          url: '/registro',
+        },
+        {
+          title: 'Login',
+          is: 'router-link',
+          url: '/login'
+        }
+      ]
+    }
+  },
+  computed: {
+    isMobile(){
+      return this.windowWidth < 550 ? true : false
+    },
+    isAuth(){
+      return this.user_data != null ? true : false
+    },
+    setItems(){
+      if(this.isMobile && this.isAuth){
+        return this.responsiveLogged
+      }else if (this.isMobile && !this.isAuth){
+        return this.responsiveOffline
+      }else return this.desktop 
     }
   },
   methods: {
-    async logout() {
-      const req = await fetch(`${process.env.VUE_APP_API_URL}usuarios/logout`);
-
-      const res = await req.json();
-
-      if(!res.ok) {
-        this.msgs = res;
-      }else {
-        this.$router.push({
-          name: "Home",
-          params: {
-            ok: res.texto,
-          },
-        });
-      }
+    onWidthChange(){
+      this.windowWidth = window.innerWidth
     },
     async checkUser() {
         const req = await fetch(`${process.env.VUE_APP_API_URL}usuarios`)
@@ -50,28 +122,44 @@ export default {
     }
   },
   mounted() {
+    window.addEventListener('resize', this.onWidthChange)
     this.checkUser()
   }
 };
 </script>
 
-<style scoped>
+<style>
 #header {
-  height: 4rem;
   width: 100%;
   background-color: #222;
   color: white;
   display: flex;
   align-items: center;
   margin-bottom: 1rem;
+  padding: 10px;
 }
 #header_links {
   display: flex;
-  flex-wrap: wrap;
   width: 100%;
+  align-items: center;
 }
-#header_links li {
-  margin-left: 30px;
+#header_links  li {
+  padding: 10px;
+  margin-left: 10px;
+  text-align: center;
+  transition: .3s ease;
+  border-radius: 10px;
+}
+#header_links li:hover{
+  background-color: #ffffff;
+  color: #222;
+}
+#imgLogo {
+  width: min(70px, 15vw);
+  border-radius: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+  cursor: pointer;
 }
 #header_user{
   display: flex;
@@ -79,6 +167,10 @@ export default {
   justify-content: flex-end;
 }
 #header_user li{
-  margin-right: 50px;
+  padding: 10px;
+  background-color: #ffffff;
+  color: #222;
+  border-radius: 10px;
+  cursor: pointer;
 }
 </style>
