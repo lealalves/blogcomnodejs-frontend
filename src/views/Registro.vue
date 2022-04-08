4c8c23acf20d37cd5e272e85eaa449863d2919f5
lealalves <leealalves@hotmail.com>
2022-04-08 19:55:29 -0300

    Update Registro.vue

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -37,45 +37,60 @@ export default {
   data() {
     return {
       msgs: null,
+      emailCheck: null,
       nome: '',
       email: '',
       senha: '',
       senha2: ''
     }
   },
+  watch: {
+    email(txt){
+
+      let usuario = txt.substring(0, txt.indexOf("@"));
+      let dominio = txt.substring(txt.indexOf("@")+ 1, txt.length);
+
+      if ((usuario.length >=1) && (dominio.length >=3) && (usuario.search("@")==-1) &&
+            (dominio.search("@")==-1) && (usuario.search(" ")==-1) && (dominio.search(" ")==-1) &&
+            (dominio.search(".")!=-1) && (dominio.indexOf(".") >=1) && 
+            (dominio.lastIndexOf(".") < dominio.length - 1)){
+            this.emailCheck = true
+          } else this.emailCheck = false
+    }
+  },
   methods: {
     async registrar() {
+      if(this.emailCheck){
 
-      const data = {
-        nome: this.nome,
-        email: this.email,
-        senha: this.senha,
-        senha2: this.senha2
-      }
-
-      const dataJson = JSON.stringify(data)
-
-
-      const req = await fetch(`${process.env.VUE_APP_API_URL}usuarios/registro`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: dataJson
-      })
-
-      const res = await req.json()
-
-      if(!res.ok){
-        this.msgs = res
-      } else {
-        this.$router.push({
-          name: 'Login',
-          params: {
-              ok: res.texto
-          }
+        const data = {
+          nome: this.nome,
+          email: this.email,
+          senha: this.senha,
+          senha2: this.senha2
+        }
+  
+        const dataJson = JSON.stringify(data)
+  
+  
+        const req = await fetch(`${process.env.VUE_APP_API_URL}usuarios/registro`, {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: dataJson
         })
-      }
-
-
+  
+        const res = await req.json()
+  
+        if(!res.ok){
+          this.msgs = res
+        } else {
+          this.$router.push({
+            name: 'Login',
+            params: {
+                ok: res.texto
+            }
+          })
+        }
+      } else this.msgs = [{texto: 'E-mail inválido.'}]
     }
   }
 }
